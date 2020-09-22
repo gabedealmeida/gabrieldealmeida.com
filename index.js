f25903cb4c07bef7f082cdb802927cc76290d2e3
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-09-22 17:24:59 -0400

    Changed bodyParser config.

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ const app = express();
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
-const jsonParser = bodyParser.json();
-
 const host = 'localhost';
 const port = 3000;
 
 app.use(express.static('public'));
 
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 app.get('/endpoint', (req, res) => {
   res.status(202).send('hello there');
 });
 
-app.post('/endpoint', jsonParser, (req, res) => {
+app.post('/endpoint', (req, res) => {
   fs.writeFileSync('endpoint.json', JSON.stringify(req.body));
   res.status(202).send();
 });
