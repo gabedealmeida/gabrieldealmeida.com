f33ab292fb9ac48194c998be6d9d14bea7e1cb99
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-27 12:48:44 -0500

    Changed the post endpooint route to only allow my personal commits to be stored in the database.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
 
-require("dotenv").config();
+require('dotenv').config();
 const port = process.env.PORT;
 const host = process.env.HOST;
 const dbURL = process.env.DATABASE_URL;
@@ -14,8 +14,8 @@ const timeAgo = new TimeAgo('en-US');
 
 const app = express();
 
-app.set("views", "./views");
-app.set("view engine", "pug");
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 
@@ -24,11 +24,15 @@ app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
   let documents = [];
-  const client = new MongoClient(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+  const client = new MongoClient(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   try {
     await client.connect();
-    documents = await client.db('gabrieldealmeida')
+    documents = await client
+      .db('gabrieldealmeida')
       .collection('github')
       .find()
       .sort({ $natural: -1 })
@@ -45,21 +49,36 @@ app.get('/', async (req, res) => {
 
 // Endpoint for github webhook
 app.post('/endpoint', async (req, res) => {
-  const client = new MongoClient(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+  const client = new MongoClient(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   const jsonObj = req.body;
   const { repository, sender } = jsonObj;
+  let data;
 
   if (jsonObj.commits) {
+    data = {
+      repo: repository.name,
+      repoPath: repository.full_name,
+      repoUrl: repository.html_url,
+      senderAvatar: sender.avatar_url,
+      senderUrl: sender.html_url,
+      commits: jsonObj.commits,
+    };
+  } else {
+    res.status(202).send();
+  }
+
+  const commits = data.commits.filter((commit) => {
+    commit.author.username === 'gabedealmeida';
+  });
+
+  if (commits.length > 0) {
+    data.commits = commits;
     try {
       await client.connect();
-      await client.db('gabrieldealmeida').collection('github').insertOne({
-        repo: repository.name,
-        repoPath: repository.full_name,
-        repoUrl: repository.html_url,
-        senderAvatar: sender.avatar_url,
-        senderUrl: sender.html_url,
-        commits: jsonObj.commits,
-      });
+      await client.db('gabrieldealmeida').collection('github').insertOne(data);
     } catch (e) {
       console.log(e);
     } finally {
